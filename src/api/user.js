a8e38ff5ce33516e9fcfca9a710a0ae6c7c5a0f8
Hoa-Tung <hoa_tung@163.com>
2019-07-11 19:11:57 +0800

    封装api请求函数

---
+++ src/api/user.js
@@ -0,0 +1,17 @@
+// 用户相关接口封装模块
+/**
+ *建议将所有的请求都封装成一个个的小函数，在需要的时候直接调用
+ 好处：好维护 统一管理 可重用
+ */
+import request from '@/utils/request'
+
+export const login = ({ mobile, code }) => {
+  request({
+    method: 'POST',
+    url: ' /app/v1_0/authorizations',
+    data: {
+      mobile,
+      code
+    }
+  })
+}
